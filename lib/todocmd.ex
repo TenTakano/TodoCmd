1a00b7d70b825514114ade9b17cd46d4d8b261a2
TenTakano <takano20771@gmail.com>
2019-06-29 01:53:54 +0900

    [update] implement parse subcommand

--- lib/todocmd.ex
+++ lib/todocmd.ex
@@ -1,3 +1,7 @@
+defmodule Task do
+  defstruct status: "", title: "", add: "", finished: "", details: [], tags: []
+end
+
 defmodule Todocmd do
   @moduledoc """
   Documentation for Todocmd.
@@ -13,14 +17,22 @@ defmodule Todocmd do
 
   """
   def main(args) do
+    %{"targetdir" => targetdir} = "./config.json" |> File.read!
+                                                  |> Poison.decode!
+
+    tasks = case File.read(targetdir) do
+              {:ok, body} -> Poison.decode!(body)
+              {:error, reason} -> [] 
+            end
+
     [subcommand | args] = args
-    case subcommand do
-      "add"     -> IO.puts "add command"
-      "done"    -> IO.puts "done command"
-      "cancel"  -> IO.puts "cancel command"
-      "mod"     -> IO.puts "mod command"
-      "flush"   -> IO.puts "flush command"
-      "list"    -> IO.puts "list command"
-    end
+    tasks = case subcommand do
+              "add"     -> IO.puts "add command"
+              "done"    -> IO.puts "done command"
+              "cancel"  -> IO.puts "cancel command"
+              "mod"     -> IO.puts "mod command"
+              "flush"   -> IO.puts "flush command"
+              "list"    -> IO.puts "list command"
+            end
   end
 end
