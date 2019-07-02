b2839100fa9219ef3b0442f9b71eb6d6a2068fea
ShoriKano <takano20771@gmail.com>
2019-07-02 23:46:05 +0900

    Divide codes

--- lib/todocmd.ex
+++ lib/todocmd.ex
@@ -1,14 +1,3 @@
-defmodule Ticket do
-  defstruct [:index, :status, :title, :add, :finished, :detals, :tags]
-
-  @behaviour Access
-
-  @impl Access
-  def fetch(term, key) do
-    Access.fetch(Map.from_struct(term), key)
-  end
-end
-
 defmodule Todocmd do
   @moduledoc """
   Documentation for Todocmd.
@@ -34,7 +23,7 @@ defmodule Todocmd do
 
     [subcommand | args] = args
     tasks = case subcommand do
-              "add"     -> add(args, tasks)
+              "add"     -> TaskList.add(args, tasks)
               "done"    -> IO.puts "done command"
               "cancel"  -> IO.puts "cancel command"
               "mod"     -> IO.puts "mod command"
@@ -44,23 +33,8 @@ defmodule Todocmd do
 
     result = File.write(targetdir, Poison.encode!(tasks))
     case result do
-      :ok               -> show(Enum.count(tasks), tasks)
+      :ok               -> TaskList.show(Enum.count(tasks), tasks)
       {:error, reason}  -> IO.puts(reason)
     end
-  end
-
-  def show(index, tasks) do
-    case index do
-      n when n == 0 ->
-        IO.inspect(Enum.at(tasks, index))
-      n when n > 0 ->
-        show(index - 1, tasks)
-        IO.inspect(Enum.at(tasks, index))
-    end
-  end
-
-  def add(arg, tasks) do
-    [title | arg] = arg
-    [%Ticket{title: title, add: Time.utc_now} | tasks]
   end
 end
