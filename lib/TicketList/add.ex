f0d0b75c4b21200524ebbe4dc513e26c99332e5b
ShoriKano <takano20771@gmail.com>
2019-07-28 16:50:17 +0900

    add license

--- lib/TicketList/add.ex
+++ lib/TicketList/add.ex
@@ -1,3 +1,7 @@
+# Copyright(c) 2019 TenTakano
+# All rights reserved.
+# See License in the project root for license information.
+
 defmodule TicketList.Add do
   def exec(args, tickets) do
     result = parse_args(args)
